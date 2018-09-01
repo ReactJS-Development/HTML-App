0f1a8a122abea6dfb15568f9c1e56e9056a1cd90
Amit umar <akuma661@marriott.com>
2018-09-01 01:32:09 -0400

    Updated server.js and created a new index.js file

---
+++ src/index.js
@@ -0,0 +1,5 @@
+console.log('I am inside index.js');
+
+const firstNumber = 10;
+
+window.number = firstNumber;
--- src/server.js
+++ src/server.js
@@ -1,1 +1,1 @@
-console.log('this is a test file');
+console.log('this is a test file123');
